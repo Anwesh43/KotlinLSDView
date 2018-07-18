2a8c2c5cb64a98ede38768dbdaf0abddda7f3224
Anwesh43 <anweshthecool0@gmail.com>
2018-07-18 09:50:12 +0530

    adding LSDView class

--- lsdview/src/main/java/com/anwesh/uiprojects/lsdview/LSDView.kt
+++ lsdview/src/main/java/com/anwesh/uiprojects/lsdview/LSDView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.lsdview
 /**
  * Created by anweshmishra on 18/07/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+class LSDView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
