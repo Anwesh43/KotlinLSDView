113e5221454db55d185ea0b2feea48988c61ab2b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-18 10:31:33 +0530

    adding LSDView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedstepdecreasingview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepdecreasingview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedstepdecreasingview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lsdview.LSDView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LSDView.create(this)
     }
 }
